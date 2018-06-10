4cb79d0956c661e036fc60372110ae64938bb5e3
houchuanhao <houchuanhao@imudges.com>
2018-06-10 08:19:00 +0800

    完成了注册

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  def new
+    @user=User.new
+    flash[:info]="info测试"
+  end
+  def create
+    #debugger
+    @user=User.new(user_params)
+    if @user.save
+      #debugger
+      flash[:success]="Welcome to the Sample App!"
+      redirect_to @user
+      #redirect_to是重定向的意思
+      #redirect_to @user等价于 redirect_to user_url(@user)
+      # user_url(@user)返回结果为 "http://0.0.0.0:3000/users/7"
+      # user_path(@user)返回结果为 "/users/7"
+    else
+      #debugger
+      # @user.errors.empty?
+      # @user.errors.any?与empty相反
+      # @user.errors.count
+      #当@user不合法时，可以调用@user.errors.full_messages获取错误信息，其返回结果为一个数组
+      render 'new'
+    end
+  end
+  def show
+    @user=User.find(params[:id])
+    # debugger #Ctrl+D键退出
+  end
+
+
+  private def user_params
+    #permit许可证
+    # 其实相当于params[:user],只不过rails中为了安全，防止传过来其他参数，于是添加了许可证，只要permit里的这几个键对应的值
+    return params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def gravatar_for(user,options = { size: 80 })
+    size=options[:size]
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    return image_tag(gravatar_url, alt: user.name, class: "gravatar") #返回一个image_tag类型的视图
+  end
 end
